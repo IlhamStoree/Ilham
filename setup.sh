793d63dfd7ada686b8892023b34d5496ece73c74
IlhamStoree <140697315+IlhamStoree@users.noreply.github.com>
2023-08-31 14:34:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Link Hosting Kalian Untuk Ssh Vpn
-# http://raw.githubusercontent.com/IlhamStoree/Ilham/main/ssh"
+http://raw.githubusercontent.com/IlhamStoree/Ilham/main/ssh"
 # Link Hosting Kalian Untuk Sstp
 IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/sstp"
 # Link Hosting Kalian Untuk Ssr
