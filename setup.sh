08ca5b85557e4b697faa14d729155057df6b1e93
IlhamStoree <140697315+IlhamStoree@users.noreply.github.com>
2023-08-31 14:16:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,25 +20,25 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Link Hosting Kalian Untuk Ssh Vpn
-akbarvpn="raw.githubusercontent.com/IlhamStoree/Ilham/main/ssh"
+IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/ssh"
 # Link Hosting Kalian Untuk Sstp
-akbarvpnn="raw.githubusercontent.com/IlhamStoree/Ilham/main/sstp"
+IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/sstp"
 # Link Hosting Kalian Untuk Ssr
-akbarvpnnn="raw.githubusercontent.com/IlhamStoree/Ilham/main/ssr"
+IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/ssr"
 # Link Hosting Kalian Untuk Shadowsocks
-akbarvpnnnn="raw.githubusercontent.com/IlhamStoree/Ilham/main/shadowsocks"
+IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/shadowsocks"
 # Link Hosting Kalian Untuk Wireguard
-akbarvpnnnnn="raw.githubusercontent.com/IlhamStoree/Ilham/main/wireguard"
+IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/wireguard"
 # Link Hosting Kalian Untuk Xray
-akbarvpnnnnnn="raw.githubusercontent.com/IlhamStoree/Ilham/main/xray"
+IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/xray"
 # Link Hosting Kalian Untuk Ipsec
-akbarvpnnnnnnn="raw.githubusercontent.com/IlhamStoree/Ilham/main/ipsec"
+IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/ipsec"
 # Link Hosting Kalian Untuk Backup
-akbarvpnnnnnnnn="raw.githubusercontent.com/IlhamStoree/Ilham/main/backup"
+IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/backup"
 # Link Hosting Kalian Untuk Websocket
-akbarvpnnnnnnnnn="raw.githubusercontent.com/IlhamStoree/Ilham/main/websocket"
+IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/websocket"
 # Link Hosting Kalian Untuk Ohp
-akbarvpnnnnnnnnnn="raw.githubusercontent.com/IlhamStoree/Ilham/main/ohp"
+IlhamStoree="raw.githubusercontent.com/IlhamStoree/Ilham/main/ohp"
 
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
